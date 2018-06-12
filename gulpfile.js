2035c9d5019259ae8af49d6d534dba63a45e27cc
bhrdn <habibbahruddin1337@gmail.com>
2018-06-12 07:09:15 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    hash         = require("gulp-hash"),
+    del          = require("del")
+
+gulp.task("scss", function () {
+    del(["themes/osprey/static/styles/*.css"])
+    gulp.src("themes/osprey/static/styles/scss/main.scss")
+        .pipe(sass({outputStyle : "compressed"}))
+        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
+        .pipe(hash())
+        .pipe(gulp.dest("themes/osprey/static/styles/"))
+        .pipe(hash.manifest("cachedAssets.json"))
+        .pipe(gulp.dest("data"))
+})
+
+gulp.task("watch", ["scss"], function () {
+    gulp.watch("themes/osprey/static/styles/scss/*", ["scss"])
+})
+
+gulp.task("default", ["watch"])
